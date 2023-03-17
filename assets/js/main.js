9fff876fa6ef032a9e5409a257e7f12bdc8eb176
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-17 16:03:10 +0100

    add event for create the cells

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,26 @@ con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 
 con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
+
+
+/* steps
+-richiamo il bottone
+-creo l'evento
+-aggiungo le celle
+*/
+
+// richiamo il bottone dal DOM
+const playButtonElement = document.getElementById('play_btn');
+
+// richiamo il contenitore dove andranno aggiunte le celle
+const containerElement = document.querySelector('.container');
+
+// creo l'evento per il bottone che genera le celle
+playButtonElement.addEventListener('click', function(){
+
+   for (let i = 1; i <= 100; i++){
+      const markupEl = `<div class="cell">${i}</div>`
+
+      containerElement.insertAdjacentHTML('beforeend', markupEl);
+   }
+})
